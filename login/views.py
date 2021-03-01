5759a51606b8c2e1118e7966fb97b680918afdf3
Felipe <felipemfmayer@gmail.com>
2021-03-01 01:41:43 -0300

    first commit

---
+++ login/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+# Create your views here.
+class LoginHome(View):
+    template_name = 'login.html'
+     
+    def get(self, request):
+        # logic
+        return render(request, self.template_name)
+    
