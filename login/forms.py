d58cfcaff1e3f29c21d8ebb63a1e018a4723d7b0
Felipe <felipemfmayer@gmail.com>
2021-03-02 10:32:37 -0300

    modal part1 and form

--- login/forms.py
+++ login/forms.py
@@ -1,4 +1,10 @@
 from django import forms
-from account.models import Account
+#from account.models import Account
 
-class LoginForm(forms.ModelForm):
+#class LoginForm(forms.ModelForm):
+
+YEARS= [x for x in range(1901,2022)]
+
+class UserForm(forms.Form):
+    
+    birth_date= forms.DateField(initial="", widget=forms.SelectDateWidget(years=YEARS, attrs={'class': 'form-select form-select-lg mb-1'}))
--- login/views.py
+++ login/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import UserForm
 
 # Create your views here.
 class LoginHome(View):
     template_name = 'login.html'
+    form_class = UserForm
+    initial = {'key': 'value'}
      
     def get(self, request):
-        # logic
-        return render(request, self.template_name)
+        form = self.form_class(initial=self.initial)
+        for field in form:
+            print(field)
+        return render(request, self.template_name, {'form' : form})
 
     
